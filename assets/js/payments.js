79a49c3b7649c21ab82077f1d761403ad046056b
rasheed mohammd <146978445+Rasheedmohammad12@users.noreply.github.com>
2026-01-31 20:42:58 +0300

    Update payments.js

--- assets/js/payments.js
+++ assets/js/payments.js
@@ -1,8 +1,13 @@
+// حماية الصفحة
+if (localStorage.getItem("loggedIn") !== "true") {
+  window.location.href = "index.html";
+}
+
 const tbody = document.getElementById("paymentsBody");
 
-function renderPayments(){
-  const payments = getPayments();
-  const patients = getPatients();
+function renderPayments() {
+  const payments = getPayments();   // من storage.js
+  const patients = getPatients();   // من storage.js
 
   tbody.innerHTML = "";
 
@@ -16,6 +21,7 @@ function renderPayments(){
       <td>${p.note || ""}</td>
       <td>${new Date(p.date).toLocaleDateString()}</td>
     `;
+
     tbody.appendChild(tr);
   });
 }

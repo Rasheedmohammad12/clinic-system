30d4124ab99b039d5505f40d71153e503c2cb550
rasheed mohammd <146978445+Rasheedmohammad12@users.noreply.github.com>
2026-02-02 16:53:52 +0300

    Update payments.js

--- assets/js/payments.js
+++ assets/js/payments.js
@@ -1,24 +1,16 @@
-import { getCurrentUser, requireAuth } from "./auth.js";
-
-requireAuth();
-const user = getCurrentUser();
-
-const PAYMENTS_KEY = `payments_${user.id}`;
+const PAYMENTS_KEY = "payments";
 const tbody = document.getElementById("paymentsBody");
 
 const payments = JSON.parse(localStorage.getItem(PAYMENTS_KEY)) || [];
-
 tbody.innerHTML = "";
 
-payments.forEach(p => {
+payments.forEach(p=>{
   const tr = document.createElement("tr");
-
   tr.innerHTML = `
     <td>${p.patientName}</td>
     <td>${p.amount}</td>
     <td>${p.note || "-"}</td>
     <td>${p.date}</td>
   `;
-
   tbody.appendChild(tr);
 });

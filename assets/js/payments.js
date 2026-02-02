385e2e87f793b9026a0cc501e75d8196d62ff2b6
rasheed mohammd <146978445+Rasheedmohammad12@users.noreply.github.com>
2026-02-02 16:44:13 +0300

    Update payments.js

--- assets/js/payments.js
+++ assets/js/payments.js
@@ -14,7 +14,7 @@ payments.forEach(p => {
   const tr = document.createElement("tr");
 
   tr.innerHTML = `
-    <td>${p.patient}</td>
+    <td>${p.patientName}</td>
     <td>${p.amount}</td>
     <td>${p.note || "-"}</td>
     <td>${p.date}</td>
